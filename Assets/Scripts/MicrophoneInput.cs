16857c9c1fc281aa1954937981bae6a695dac4c1
samanehkazemi <kazemi.samaneh@gmail.com>
2017-10-07 12:58:12 -0400

    First Commit

---
+++ Assets/Scripts/MicrophoneInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MicrophoneInput : MonoBehaviour {
+	public Button record, play;
+	private AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+		//record.onClick.AddListener (Record);
+	}
+
+	public void Record(){
+		print ("recording....");
+		audio.clip = Microphone.Start ("Built-in Microphone", true, 10, 44100);
+	}
+
+	public void Play() {
+		print ("playing...");
+		audio.Play ();
+	}
+
+}
